8dda5db004cb339fc3acbcfdcbd546c92ed9d0d5
angeloremos <125968568+angeloremos@users.noreply.github.com>
2026-06-02 08:31:27 +0800

    Update api.fixture.js

--- fixtures/api.fixture.js
+++ fixtures/api.fixture.js
@@ -1,1 +1,10 @@
+import { test as fixture } from '@playwright/test'
+import ApiRequest from '../resources/apiactions/base.js'
 
+const apitest = fixture.extend({
+    api: async ({ request }, use) => {
+        await use(new ApiRequest(request))
+    }
+})
+
+export default apitest
